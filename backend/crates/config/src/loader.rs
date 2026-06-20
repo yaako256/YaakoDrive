bc01ac2f7d588091a8a7fbe21500abe343654252
yaako256 <281857787+yaako256@users.noreply.github.com>
2026-06-21 02:56:22 +0900

    refactor: Docker周辺の設定が甘かったので調整する

--- backend/crates/config/src/loader.rs
+++ backend/crates/config/src/loader.rs
@@ -19,12 +19,15 @@ pub fn load() -> ConfigResult<AppConfig> {
   // 環境変数から環境ラベルを読み込む
   let env = std::env::var("APP_ENV")?;
 
+  // 環境変数から設定ファイルがあるディレクトリパスを読み込む
+  let config_dir = std::env::var("APP_CONFIG_DIR")?;
+
   // 設定ファイルをロードする
   let settings = Config::builder()
     // 基本設定の読み込み(TOML)
-    .add_source(File::with_name("../.config/config.default.toml").required(true))
+    .add_source(File::with_name(&format!("{config_dir}/config.default.toml")).required(true))
     // 環境ラベル別上書き設定の読み込み(TOML)
-    .add_source(File::with_name(&format!("../.config/config.{env}.toml")).required(true))
+    .add_source(File::with_name(&format!("{config_dir}/config.{env}.toml")).required(true))
     // 2. ENV上書き（APP__AAAAA__BBBB_BBB_BBBB形式）
     .add_source(Environment::with_prefix("APP").separator("__"))
     .build()?;
